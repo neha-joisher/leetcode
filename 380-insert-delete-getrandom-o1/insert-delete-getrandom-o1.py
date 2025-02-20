73f109e0aeb7c428516587582c229319ab571990
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-02-20 18:01:48 -0500

    Time: 141 ms (50.96%) | Memory: 57.8 MB (86.67%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,26 +1,39 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.RandomizedSet=set()
+        self.data=[]
+        self.indices={}
         
     def insert(self, val: int) -> bool:
-        if val in self.RandomizedSet:
+        if val in self.indices:
             return False
         else:
-            self.RandomizedSet.add(val)
+            index=len(self.data)
+            self.indices[val]=index
+            self.data.append(val)
             return True
-        
 
     def remove(self, val: int) -> bool:
-        if val in self.RandomizedSet:
-            self.RandomizedSet.remove(val)
-            return True
-        else:
+        if val not in self.indices:
             return False
-        
+        else:
+            # Find index of val and get the last element
+            index = self.indices[val]
+            last_element = self.data[-1]
+
+            # Swap val with the last element in O(1)
+            self.data[index] = last_element
+            self.indices[last_element] = index
+
+            # Remove the last element from data and delete val from indices
+            self.data.pop()
+            del self.indices[val]
+            return True
 
     def getRandom(self) -> int:
-        return random.choice(list(self.RandomizedSet))
+        return random.choice(self.data)
+        
+
         
 
 
