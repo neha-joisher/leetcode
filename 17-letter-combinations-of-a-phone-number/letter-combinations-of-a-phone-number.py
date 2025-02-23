29b9d52291a0f8be44c33f0b67ca4b87626cf9ab
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-02-22 19:16:29 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.86%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        phone_map = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+
+        # Return an empty list if the input is empty
+        if not digits:
+            return []
+
+        result = []
+        sol=[]
+
+        # Backtracking function to build combinations
+        def backtrack(index, path):
+            # If the current combination is complete, add it to the result
+            if index == len(digits):
+                result.append("".join(sol))
+                return
+
+            # Iterate over the possible letters for the current digit
+            for letter in phone_map[digits[index]]:
+                sol.append(letter)        # Choose
+                backtrack(index + 1, path) # Explore
+                sol.pop()                # Un-choose (backtrack)
+
+        backtrack(0, sol)
+        return result
+        
