aab26864d0e76eb98f34200a6f50f969aab3ea8a
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-02-25 19:05:50 -0500

    Time: 42 ms (48.52%) | Memory: 17.5 MB (89.12%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -8,14 +8,28 @@ class Solution:
         #     return 1
         # return self.fib(n-1)+self.fib(n-2)
 
-        memo={0:0, 1:1}
-        def f(x):
-            if x in memo:
-                return memo[x]
-            else:
-                memo[x]=f(x-1)+f(x-2)
-                return memo[x]
-        return f(n)
+        # 2. Top Down - Mmemoization
+        # memo={0:0, 1:1}
+        # def f(x):
+        #     if x in memo:
+        #         return memo[x]
+        #     else:
+        #         memo[x]=f(x-1)+f(x-2)
+        #         return memo[x]
+        # return f(n)
+
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        print(dp)
+        return dp[n]
+
 
 
         
