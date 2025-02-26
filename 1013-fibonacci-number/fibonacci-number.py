438371e2ad79da86cdcf7386cc58fe1c5f85c29f
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-02-25 19:12:14 -0500

    Time: 40 ms (58.88%) | Memory: 17.9 MB (24.09%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -7,6 +7,8 @@ class Solution:
         # if n==1:
         #     return 1
         # return self.fib(n-1)+self.fib(n-2)
+        # Time Complexity is O(2^n)
+
 
         # 2. Top Down - Mmemoization
         # memo={0:0, 1:1}
@@ -17,18 +19,36 @@ class Solution:
         #         memo[x]=f(x-1)+f(x-2)
         #         return memo[x]
         # return f(n)
+        # Time and Space Complexity is O(n)
+
+
+        # 3. Bottom Up 
+        # you need this base cases because what if the input is 0 or 1, the loop will give index out of bound error
+        # if n==0:
+        #     return 0
+        # if n==1:
+        #     return 1
+        # dp=[0]*(n+1)
+        # dp[0]=0
+        # dp[1]=1
+        # for i in range(2,n+1):
+        #     dp[i]=dp[i-1]+dp[i-2]
+        # print(dp)
+        # return dp[n]
+        # Time and Space Complexity is O(n)
 
         if n==0:
             return 0
         if n==1:
             return 1
-        dp=[0]*(n+1)
-        dp[0]=0
-        dp[1]=1
+
+        prev=0
+        curr=1
         for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        print(dp)
-        return dp[n]
+            prev,curr=curr,curr+prev
+        return curr
+
+
 
 
 
