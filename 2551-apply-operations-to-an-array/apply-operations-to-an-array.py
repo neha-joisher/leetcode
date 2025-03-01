8ef4efcda5539bdc98740c3083f7bbf3df57f5e5
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-01 18:39:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (75.05%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,18 +1,39 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        res=[]
-        for i in range(len(nums)):
-            if i!=len(nums)-1:
+        for i in range(len(nums)-1):
+            # if i!=len(nums)-1:
                 if nums[i]==nums[i+1]:
-                    nums[i]=nums[i]*2
-                    nums[i+1]=0
+                        nums[i]=nums[i]*2
+                        nums[i+1]=0
+        temp=0 
+        #temp variable will keep track of where the next non zero index is
+        for i in range(len(nums)):
             if nums[i]!=0:
-                res.append(nums[i])
-        len_res=len(res)
-        len_nums=len(nums)
-        count=len_nums-len_res
-        res.extend([0]*(count))
-        return res
+                nums[temp]=nums[i]
+                temp+=1
+        for i in range(temp,len(nums)):
+            nums[i]=0
+        return nums
+
+
+
+        
+            
+
+
+        # res=[]
+        # for i in range(len(nums)-1):
+        #     if i!=len(nums)-1:
+        #         if nums[i]==nums[i+1]:
+        #             nums[i]=nums[i]*2
+        #             nums[i+1]=0
+        #         if nums[i]!=0:
+        #             res.append(nums[i])
+        # len_res=len(res)
+        # len_nums=len(nums)
+        # count=len_nums-len_res
+        # res.extend([0]*(count))
+        # return res
 
 
 
