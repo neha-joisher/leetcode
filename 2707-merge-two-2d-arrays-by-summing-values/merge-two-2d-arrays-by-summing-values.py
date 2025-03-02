1a0563881bf546db5357e74e70745f5c3e143223
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-02 16:03:34 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (62.24%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -8,22 +8,14 @@ class Solution:
                 res.append([nums1[l1][0],add])
                 l1+=1
                 l2+=1
-                print(res)
             elif nums1[l1][0]<nums2[l2][0]:
                 res.append(nums1[l1])
                 l1+=1
-                print(res)
             elif nums1[l1][0]>nums2[l2][0]:
                 res.append(nums2[l2])
                 l2+=1
-                print(res)
-        while(l1<len(nums1) or l2<len(nums2)):
-            if l1<len(nums1):
-                res.append(nums1[l1])
-                l1+=1
-            else:
-                 res.append(nums2[l2])
-                 l2+=1
+        res.extend(nums1[l1:])
+        res.extend(nums2[l2:])
         return res
 
 
