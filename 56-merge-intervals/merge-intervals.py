a894af06940796c1aaaf2a832e55e82d89772cf2
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-08 18:51:59 -0500

    Time: 7 ms (73.04%) | Memory: 20.8 MB (97.20%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,16 +1,32 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        i=0
-        j=1
-        res=[]
-        intervals.sort()
-        while j <len(intervals):
-            if intervals[i][1] < intervals[j][0]:
-                res.append(intervals[i])
+        if not intervals:
+            return []
+    
+        intervals.sort()  # Sort intervals by start time
+        res = [intervals[0]]  # Start with the first interval
+        
+        for i in range(1, len(intervals)):
+            prev = res[-1]  # Last interval in the result list
+            curr = intervals[i]  # Current interval
+            
+            if prev[1] >= curr[0]:  # Overlapping intervals
+                res[-1] = [prev[0], max(prev[1], curr[1])]
             else:
-                intervals[j]=[min(intervals[i][0],intervals[j][0]),max(intervals[i][1],intervals[j][1])]
-            i=j
-            j+=1
-        res.append(intervals[j-1])
+                res.append(curr)
+        
         return res
+        # i=0
+        # j=1
+        # res=[]
+        # intervals.sort()
+        # while j <len(intervals):
+        #     if intervals[i][1] < intervals[j][0]:
+        #         res.append(intervals[i])
+        #     else:
+        #         intervals[j]=[min(intervals[i][0],intervals[j][0]),max(intervals[i][1],intervals[j][1])]
+        #     i=j
+        #     j+=1
+        # res.append(intervals[j-1])
+        # return res
 #O(NLogN)
