404ecc4b211af8f0155a4287cac575b49271c3c4
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-09 15:20:46 -0400

    Time: 0 ms (100.00%) | Memory: 19.9 MB (75.59%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,20 +1,31 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if not intervals:
-            return 0
+
+
         intervals.sort()
-        res=[intervals[0]]
-        j=1
-        flag=False
-        while(j<len(intervals)):
-            flag=False
-            for i,x in enumerate(res):
-                if len(x)==2 and x[-1]<=intervals[j][0] or len(x)>2 and x[-1][1]<=intervals[j][0]:
-                    flag=True
-                    res[i].append(intervals[j])
-                    break
-            if flag==False:
-                res.append(intervals[j])
-            j+=1
-        return len(res)
+        minheap=[]
+
+        for i in intervals:
+            if minheap and minheap[0]<=i[0]:
+                heapq.heappop(minheap)
+            heapq.heappush(minheap,i[1])
+        return len(minheap)
+
+        # if not intervals:
+        #     return 0
+        # intervals.sort()
+        # res=[intervals[0]]
+        # j=1
+        # flag=False
+        # while(j<len(intervals)):
+        #     flag=False
+        #     for i,x in enumerate(res):
+        #         if len(x)==2 and x[-1]<=intervals[j][0] or len(x)>2 and x[-1][1]<=intervals[j][0]:
+        #             flag=True
+        #             res[i].append(intervals[j])
+        #             break
+        #     if flag==False:
+        #         res.append(intervals[j])
+        #     j+=1
+        # return len(res)
         
