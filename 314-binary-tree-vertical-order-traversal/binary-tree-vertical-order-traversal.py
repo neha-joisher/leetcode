6b38d652b4e294c884870cd96d0ec9fade378d3a
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-09 20:04:28 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (23.71%) - LeetSync

---
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue=collections.deque()
+        queue.append([0,root])
+        hashmap_level=defaultdict(list)
+        min_col=float("inf")
+        max_col=float("-inf")
+        res=[]
+        while queue:
+            level,root=queue.popleft()
+            hashmap_level[level].append(root.val)
+            if level < min_col: min_col=level
+            if level > max_col: max_col=level
+            if root.left: queue.append([level-1,root.left])
+            if root.right: queue.append([level+1,root.right])
+
+        for i in range(min_col,max_col+1):
+            res.append(hashmap_level[i])
+        return res
+
