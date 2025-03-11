4f756f7acb07df9e9ba57c44a62e2e5f5cf8f2f9
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-11 12:19:17 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (71.79%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,21 +3,35 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        if n==0:
-            return
-        p,q=0,0
-        while(p<len(nums1)):
-            if m!=0 and n!=0 and nums1[p]<=nums2[q] :
-                    p+=1
-                    m-=1
+        if n == 0:
+            return  # No elements to merge, return immediately
+    
+        p, q = 0, 0  # Pointers for nums1 and nums2
+            
+        while p < m + n and q < n:
+            if p < m and nums1[p] <= nums2[q]:
+                    p += 1
             else:
-                if n==0:
-                    return
-                nums1.insert(p,nums2[q])
-                p+=1
-                nums1.pop()
-                q+=1
-                n-=1
+                nums1.insert(p, nums2[q])  # Insert element from nums2 into nums1
+                nums1.pop()  # Remove the last element to maintain the correct size
+                q += 1
+                p += 1
+                m += 1
+        # if n==0:
+        #     return
+        # p,q=0,0
+        # while(p<len(nums1)):
+        #     if m!=0 and n!=0 and nums1[p]<=nums2[q] :
+        #             p+=1
+        #             m-=1
+        #     else:
+        #         if n==0:
+        #             return
+        #         nums1.insert(p,nums2[q])
+        #         p+=1
+        #         nums1.pop()
+        #         q+=1
+        #         n-=1
 
 
         
