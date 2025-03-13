05868f99e0d5b46afbc41bfe100199dd200891e3
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-13 16:06:54 -0400

    Time: 403 ms (6.90%) | Memory: 18.1 MB (48.67%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,30 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        if not s:
+            return 0
+        i=j=0
+        max_len=1
+        hashmap={}
+
+        def get_max_freq():
+            count=int(max(hashmap.values()))
+            return count
+
+        while(j<len(s)):
+            if s[j] in hashmap:
+                hashmap[s[j]]=hashmap[s[j]]+1
+            else:
+                hashmap[s[j]]=1
+
+            max_freq=get_max_freq()
+            print(max_freq,type(max_freq))
+            if ((j-i)+1)-max_freq<=k:
+                max_len=max(max_len,((j-i)+1))
+                j+=1
+            else:
+                hashmap[s[i]]=hashmap[s[i]]-1
+                i+=1
+                j+=1
+        return max_len
+
+        
