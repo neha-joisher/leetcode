427ef3d23515f2892377a9f7f4496dc89db09321
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-13 16:32:25 -0400

    Time: 143 ms (45.75%) | Memory: 18 MB (48.67%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,24 +6,19 @@ class Solution:
         max_len=1
         hashmap={}
 
-        def get_max_freq():
-            count=int(max(hashmap.values()))
-            return count
-
         while(j<len(s)):
-            if s[j] in hashmap:
-                hashmap[s[j]]=hashmap[s[j]]+1
-            else:
-                hashmap[s[j]]=1
 
-            max_freq=get_max_freq()
+            hashmap[s[j]]=1+hashmap.get(s[j],0)
+
+            max_freq=max(hashmap.values())
             if ((j-i)+1)-max_freq<=k:
                 max_len=max(max_len,((j-i)+1))
                 j+=1
             else:
-                hashmap[s[i]]=hashmap[s[i]]-1
+                hashmap[s[i]]-=1
                 i+=1
                 j+=1
         return max_len
+
 
         
