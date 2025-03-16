9adaa37935b95332b44bb8f2eb46e2bf71bc9aeb
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-16 08:05:37 -0700

    Time: 3 ms (98.45%) | Memory: 18.1 MB (25.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,20 +1,25 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hashmap_s={}
-        hashmap_t={}
 
-        def get_count(hashmap,st):
-            for x in st:
-                if x in hashmap:
-                    hashmap[x]=hashmap[x]+1
-                else:
-                    hashmap[x]=1
-            return hashmap
-        hashmap_s= get_count(hashmap_s,s)
-        hashmap_t= get_count(hashmap_t,t)
+        freq_Counter_S=Counter(s)
+        freq_Counter_T=Counter(t)
+        return freq_Counter_S==freq_Counter_T
 
-        if hashmap_s==hashmap_t:
-            return True
-        else:
-            return False
+        # hashmap_s={}
+        # hashmap_t={}
+
+        # def get_count(hashmap,st):
+        #     for x in st:
+        #         if x in hashmap:
+        #             hashmap[x]=hashmap[x]+1
+        #         else:
+        #             hashmap[x]=1
+        #     return hashmap
+        # hashmap_s= get_count(hashmap_s,s)
+        # hashmap_t= get_count(hashmap_t,t)
+
+        # if hashmap_s==hashmap_t:
+        #     return True
+        # else:
+        #     return False
         
