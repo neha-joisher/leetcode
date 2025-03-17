e16a326f931e73b7e646cb3c4aa2bae6f911285e
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-16 18:52:05 -0700

    Time: 798 ms (43.31%) | Memory: 18.1 MB (87.73%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,40 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp=[amount+1]*(amount+1)
+        dp[0]=0
+
+        for i in range(1,amount+1):
+            for c in coins:
+                if i-c>=0:
+                    dp[i]=min(dp[i],dp[i-c]+1)
+        return dp[amount] if dp[amount]!=amount+1 else -1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # min_coin=float("inf")
+        # res=[]
+        # sol=[]
+        # def dfs(add):
+        #     nonlocal min_coin
+        #     if len(sol) >= min_coin:
+        #         return
+
+        #     if add==amount:
+        #         if len(sol)<min_coin:
+        #             min_coin=len(sol)
+        #             return 
+        #     if add>amount:
+        #         return
+        #     for i in coins:
+        #         sol.append(i)
+        #         dfs(add+i)
+        #         sol.pop()
+        # dfs(0)
+        # result = min_coin if min_coin != float("inf") else -1
+        # return result
