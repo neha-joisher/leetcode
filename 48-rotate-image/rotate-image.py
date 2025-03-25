67bd063167163b3633f491b4bdbb92ab1da95ff1
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-25 16:14:03 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (23.10%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,8 +4,9 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
 
+
         n=len(matrix)
-        # Step 1: Transpose (swap matrix[i][j] with matrix[j][i])
+        # Step 1: Transpose (Swap the upper half of the diagonal with the lower half)
         for i in range(n):
             for j in range(i + 1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
