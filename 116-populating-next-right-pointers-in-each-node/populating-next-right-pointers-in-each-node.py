da27d92b6567b52ddeb33900a93652466f3f2f32
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-26 11:04:12 -0700

    Time: 49 ms (71.54%) | Memory: 19.4 MB (5.93%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -11,19 +11,35 @@ class Node:
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
         if not root:
-             return None
-        queue=collections.deque()
-        queue.append(root)
-
-        while queue:
-            len_queue=len(queue)
-
-            for i in range(len_queue):
-                current=queue.popleft()
-                if i<len_queue-1:
-                    current.next=queue[0]
-                if current.left: queue.append(current.left)
-                if current.right: queue.append(current.right)
+            return None
         
+        curr,nextCurr=root, root.left if Node else None
+
+        while curr and nextCurr:
+            curr.left.next=curr.right
+            if curr.next:
+                curr.right.next=curr.next.left
+            curr=curr.next
+            if not curr:
+                curr=nextCurr
+                nextCurr=curr.left
         return root
+
+
+        # if not root:
+        #     return None
+        # queue=collections.deque()
+        # queue.append(root)
+
+        # while queue:
+        #     len_queue=len(queue)
+
+        #     for i in range(len_queue):
+        #         current=queue.popleft()
+        #         if i<len_queue-1:
+        #             current.next=queue[0]
+        #         if current.left: queue.append(current.left)
+        #         if current.right: queue.append(current.right)
+        
+        # return root
         
