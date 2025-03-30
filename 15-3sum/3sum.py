69459d06c529f08183ebb69c63d09c943018ab9b
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-03-30 10:29:31 -0700

    Time: 667 ms (79.66%) | Memory: 18.9 MB (23.14%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,55 +1,26 @@
-class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res=[]
         nums.sort()
-
-        # num_set=set(nums)
-        # l,r=0,len(nums)-1
-        # while(l<r):
-        #     add=(nums[l]+nums[r])
-        #     if -add in num_set:
-        #         res.append([nums[l],nums[r],-add])
-        #     l+=1
-        #     r-=1
-        # print(res)
-        # return res
-
         for i, a in enumerate(nums):
-            if a > 0:
-                break
-
             if i > 0 and a == nums[i - 1]:
-                continue
-
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                threeSum = a + nums[l] + nums[r]
-                if threeSum > 0:
-                    r -= 1
-                elif threeSum < 0:
-                    l += 1
+                    continue 
+            l=i+1
+            r=len(nums)-1
+            while(l<r):
+                if nums[l]+nums[r]+a>0:
+                    r-=1
+                elif nums[l]+nums[r]+a<0:
+                    l+=1
                 else:
-                    res.append([a, nums[l], nums[r]])
+                    res.append([a,nums[l],nums[r]])
                     l += 1
                     r -= 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-                    while nums[r] == nums[r+ 1] and l < r:
-                        r -= 1
-                        
         return res
             
-
-
-        # res=[]
-        # for i in range(len(nums)-2):
-        #     for j in range(i+1, len(nums)-1):
-        #         for k in range(j+1, len(nums)):
-        #             if (nums[i]+nums[j]+nums[k]==0):
-        #                 res.append([nums[i],nums[j],nums[k]])
-
-        # print(res)
-        # return res
-
-
-        
