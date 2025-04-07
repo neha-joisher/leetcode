523243f063f14c6b45dcbd0ea48e3a12e701ddbe
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-04-06 18:07:50 -0700

    Time: 277 ms (20.10%) | Memory: 13.2 MB (42.64%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -9,25 +9,51 @@ class Solution(object):
         if len(t)>len(s):
             return ''
         freq_Counter_t=Counter(t)
+        need=len(freq_Counter_t)
+        have=0
         freq_Counter_s={}
         st=e=0
         min_substring=''
         min_length=float('inf')
-        def window_is_valid():
-            for ch in freq_Counter_t:
-                if freq_Counter_s.get(ch, 0) < freq_Counter_t[ch]:
-                    return False
-            return True
         while e<len(s):#or window is valid
             if s[e] in freq_Counter_t:
                 freq_Counter_s[s[e]]=freq_Counter_s.get(s[e],0)+1
-            while window_is_valid():#window is valid
+                if freq_Counter_s[s[e]]==freq_Counter_t[s[e]]:
+                    have+=1
+            while have==need:#window is valid
                 if min(min_length,e-st+1)==e-st+1:
                     min_length=e-st+1
                     min_substring=s[st:e+1]
                 if s[st] in freq_Counter_t:
                     freq_Counter_s[s[st]] -= 1
+                    if freq_Counter_s[s[st]]<freq_Counter_t[s[st]]:
+                        have-=1
                 st += 1
             e+=1
         return min_substring
+
+        # if len(t)>len(s):
+        #     return ''
+        # freq_Counter_t=Counter(t)
+        # freq_Counter_s={}
+        # st=e=0
+        # min_substring=''
+        # min_length=float('inf')
+        # def window_is_valid():
+        #     for ch in freq_Counter_t:
+        #         if freq_Counter_s.get(ch, 0) < freq_Counter_t[ch]:
+        #             return False
+        #     return True
+        # while e<len(s):#or window is valid
+        #     if s[e] in freq_Counter_t:
+        #         freq_Counter_s[s[e]]=freq_Counter_s.get(s[e],0)+1
+        #     while window_is_valid():#window is valid
+        #         if e-st+1<min_length:
+        #             min_length=e-st+1
+        #             min_substring=s[st:e+1]
+        #         if s[st] in freq_Counter_t:
+        #             freq_Counter_s[s[st]] -= 1
+        #         st += 1
+        #     e+=1
+        # return min_substring
         
