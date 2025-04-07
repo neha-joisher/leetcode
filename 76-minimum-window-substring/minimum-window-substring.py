360eaa0c11f2bb1c527109db2437ba27fe1c16dd
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-04-06 17:36:33 -0700

    Time: 1332 ms (5.00%) | Memory: 12.7 MB (77.83%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+
+        if len(t)>len(s):
+            return ''
+        freq_Counter_t=Counter(t)
+        freq_Counter_s={}
+        st=e=0
+        min_substring=''
+        min_length=float('inf')
+        def window_is_valid():
+            for ch in freq_Counter_t:
+                if freq_Counter_s.get(ch, 0) < freq_Counter_t[ch]:
+                    return False
+            return True
+        while e<len(s):#or window is valid
+            if s[e] in freq_Counter_t:
+                freq_Counter_s[s[e]]=freq_Counter_s.get(s[e],0)+1
+            while window_is_valid():#window is valid
+                if min(min_length,e-st+1)==e-st+1:
+                    min_length=e-st+1
+                    min_substring=s[st:e+1]
+                if s[st] in freq_Counter_t:
+                    freq_Counter_s[s[st]] -= 1
+                st += 1
+            e+=1
+        return min_substring
+        
