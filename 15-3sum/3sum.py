19583f59b5151e765cb34facb38e91a592b7046b
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-04-19 20:01:06 -0700

    Time: 674 ms (77.48%) | Memory: 18.3 MB (63.16%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -6,21 +6,40 @@ class Solution(object):
         """
         res=[]
         nums.sort()
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i - 1]:
-                    continue 
+        for i,n in enumerate(nums):
+            if i!=0 and nums[i]==nums[i-1]:
+                continue
             l=i+1
             r=len(nums)-1
             while(l<r):
-                if nums[l]+nums[r]+a>0:
+                if n+nums[l]+nums[r]>0:
                     r-=1
-                elif nums[l]+nums[r]+a<0:
+                elif n+nums[l]+nums[r]<0:
                     l+=1
                 else:
-                    res.append([a,nums[l],nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
+                    res.append([n,nums[l],nums[r]])
+                    l+=1
+                    while nums[l]==nums[l-1] and l<r:
+                        l+=1
+                    r-=1
         return res
+        # res=[]
+        # nums.sort()
+        # for i, a in enumerate(nums):
+        #     if i > 0 and a == nums[i - 1]:
+        #             continue 
+        #     l=i+1
+        #     r=len(nums)-1
+        #     while(l<r):
+        #         if nums[l]+nums[r]+a>0:
+        #             r-=1
+        #         elif nums[l]+nums[r]+a<0:
+        #             l+=1
+        #         else:
+        #             res.append([a,nums[l],nums[r]])
+        #             l += 1
+        #             r -= 1
+        #             while nums[l] == nums[l - 1] and l < r:
+        #                 l += 1
+        # return res
             
