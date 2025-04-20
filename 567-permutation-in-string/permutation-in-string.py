b2b81a44ce9480ea3c0471d672e9cfa056c953b4
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-04-20 13:49:08 -0700

    Time: 25 ms (68.72%) | Memory: 12.7 MB (64.24%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,13 +5,38 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        freqMap1=Counter(s1)
-        for i in range(len(s2)-len(s1)+1):
-            if s2[i] in freqMap1:
-                freqMap2=Counter(s2[i:i+len(s1)])
-                if freqMap1==freqMap2:
-                    return True
-        return False
+        if len(s1) > len(s2):
+            return False
+        
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord('a')] += 1
+            s2Count[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+        
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1
+            l += 1
+        return matches == 26
 
         
 
