4529ee8df8eedee1ab274569347e062a888d5f13
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-04-21 16:02:55 -0700

    Time: 340 ms (5.26%) | Memory: 38.2 MB (59.93%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,19 +1,12 @@
-class Solution(object):
-    def carFleet(self, target, position, speed):
-        """
-        :type target: int
-        :type position: List[int]
-        :type speed: List[int]
-        :rtype: int
-        """
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         cars=[(p,s) for p, s in zip(position,speed)]
         cars.sort(reverse=True)
         stack=[]
 
         for p,s in cars:
-            time=(float(target-p)/s)
+            time=((target-p)/s)
             print(time)
             if not stack or time > stack[-1]:
                 stack.append(time)  # New fleet formed
         return len(stack)
-        
