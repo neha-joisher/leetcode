54d38b50c90ae4673d1866eea51454c071362d09
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-05-14 17:57:47 -0700

    Time: 7 ms (31.66%) | Memory: 19 MB (27.81%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -6,16 +6,15 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        if root is None:
-            return True
-        left=self.height(root.left)
-        right=self.height(root.right)
-        if abs(left - right) > 1:
-            return False
-        return self.isBalanced(root.left) and self.isBalanced(root.right)
-        
-    def height(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(root):
             if not root:
-                return 0
-            return 1+max(self.height(root.left),self.height(root.right))        
+                return [True,0]
+            
+            left,right= dfs(root.left),dfs(root.right)
+            balanced=left[0] and right[0] and abs(left[1]-right[1])<=1
+
+            return [balanced,1+max(left[1], right[1])]
+
+        return dfs(root)[0]
         
