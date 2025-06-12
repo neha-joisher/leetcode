45337e53ef77a0b18f6cebdb0668c2035369d127
Neha Joisher <114249632+neha-joisher@users.noreply.github.com>
2025-06-12 19:35:54 -0400

    Time: 11 ms (59.06%) | Memory: 43.3 MB (85.73%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cs
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cs
@@ -4,13 +4,15 @@ public class Solution {
         int start=0;
         int end=0;
         int maxLength=0;
+        int mostFreqCharCount=0;
         while(end<s.Length){
             if(freqMap.ContainsKey(s[end])){
                 freqMap[s[end]]++;
             }else{
                 freqMap[s[end]]=1;
             }
-            var mostFreqCharCount=freqMap.OrderByDescending(kvp=>kvp.Value).Select(kvp=>kvp.Value).First();
+            mostFreqCharCount=Math.Max(mostFreqCharCount,freqMap[s[end]]);
+            // var mostFreqCharCount=freqMap.OrderByDescending(kvp=>kvp.Value).Select(kvp=>kvp.Value).First();
             if((end-start+1)-mostFreqCharCount>k){
                 freqMap[s[start]]--;
                 start++;
